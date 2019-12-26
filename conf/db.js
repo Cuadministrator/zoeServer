25df47ef176674c397501c8e34a6ddac8ee987d3
kaijie.tang <kaijie.tang@aihuishou.com>
2019-12-26 22:27:29 +0800

    init

--- conf/db.js
+++ conf/db.js
@@ -4,8 +4,8 @@ module.exports = {
   mysql: {
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
-    database: 'research',
+    password: '123qweasd',
+    database: 'zoeServer',
     port: 3306
   }
 };
